102da9f917630637f0ab7d52bca933772d9e2b44
jemmyjohnes <43142384+jemmyjohnes@users.noreply.github.com>
2018-09-11 00:54:25 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,20 @@
+/*
+ * Discord Bot Builder Bot
+ * Version 1.2.0
+ * Robert Borghese
+ */
+
+const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
+
+if(!process.send) {
+
+Files.initStandalone();
+
+} else {
+
+process.on('message', function(content) {
+	Files.initBotTest(content);
+});
+client.login(process.env.NDcxNzU0MzkxOTE4MjE1MTY4.Dng1cA.dWVM-fFHmZnEkjJEqHCSX_vZZ5Y);
+
+}
